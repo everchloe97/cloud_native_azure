27a5f8ea4a2363d01ace7781a3bc24d3b82a3c7a
haneul <haneul@tecace.com>
2023-10-11 12:32:14 +0900

    Remove / 미사용 files

--- Chapter9/azure/eventhub/go/recv/go.mod
+++
@@ -1,5 +0,0 @@
-module cnia/recv
-
-go 1.16
-
-require github.com/Azure/azure-event-hubs-go v1.3.1 // indirect
--- Chapter9/azure/eventhub/terraform/show-ehub-conn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-terraform plan -out=myplan > /dev/null
-terraform show -json myplan | jq '.planned_values.root_module.resources|.[2].values'
-
-rm -vf myplan
